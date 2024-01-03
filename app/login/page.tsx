dade33f4dba67b534ba9999adce51e73c07a200d
corentinparp <corentin.parpette@accenture.com>
2024-01-03 16:26:29 +0100

    fix: sign in button in mobile device

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
             await signIn("github", { callbackUrl: "/" });
           }}>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <div className="hidden md:block">
+            <div className="">
               Sign In With GitHub
             </div>
           </button>
